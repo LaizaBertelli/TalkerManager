939556d286ef1abf0ff0be3522e34a0d457e94a6
LaizaBertelli <laizabertelli@hotmail.com>
2022-02-16 17:13:32 -0300

    feat: implements middleeware getTalkerById

--- index.js
+++ index.js
@@ -14,12 +14,25 @@ async function getAllTalkers(req, res) {
   res.status(200).send(JSON.parse(talkers));
 }
 
+// Requisito 2 - endpoint GET /talker/:id
+async function getTalkerById(req, res) {
+  const { id } = req.params;
+  const talkers = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
+  const filteredTalkers = talkers.find((talker) => talker.id === Number(id));
+  if (filteredTalkers) {
+    res.status(200).send(filteredTalkers);
+  } else {
+    res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app
   .get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 })
-  .get('/talker', (req, res) => getAllTalkers(req, res));
+  .get('/talker', (req, res) => getAllTalkers(req, res))
+  .get('/talker/:id', (req, res) => getTalkerById(req, res));
 
 app.listen(PORT, () => {
   console.log('Online');
