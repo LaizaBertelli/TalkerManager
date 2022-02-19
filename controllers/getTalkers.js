ecdcaf7645fd23a9b1b4a7bcb91c22001af9e124
LaizaBertelli <laizabertelli@hotmail.com>
2022-02-19 19:39:43 -0300

    fix: fixes lint errors

--- controllers/getTalkers.js
+++ controllers/getTalkers.js
@@ -1,12 +1,12 @@
 const { readFile } = require('fs/promises');
+
 const PATH = './talker.json';
 
 module.exports = async (req, res, next) => {
   try {
     const talkers = JSON.parse(await readFile(PATH, 'utf-8'));
-
     return res.status(200).json(talkers);
-  } catch(e) {
+  } catch (e) {
     return next(e);
   }
 };
