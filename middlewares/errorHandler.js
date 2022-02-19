5ca1947eccfd7793f57331068da1f93bb3de38ea
LaizaBertelli <laizabertelli@hotmail.com>
2022-02-19 19:07:55 -0300

    fix: fixes lint errors

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,3 +1,1 @@
-module.exports = (_req, res, _next) => {
-  return res.status(500).json({ message: 'Something went wrong' });
-};
+module.exports = (_req, res, _next) => res.status(500).json({ message: 'Something went wrong' });
