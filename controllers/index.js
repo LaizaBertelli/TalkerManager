8c4eba496e68d5e9aa4ae7392835e7ec01ddbd58
LaizaBertelli <laizabertelli@hotmail.com>
2022-02-19 19:09:08 -0300

    feat: add middlewares and controllers to its indexes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const getTalkers = require('./getTalkers');
 const getTalkerById = require('./getTalkerById');
+const newlogin = require('./newLogin');
+const createTalker = require('./createTalker');
 
 module.exports = {
   getTalkers,
   getTalkerById,
+  newlogin,
+  createTalker,
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,9 @@
 const errorHandler = require('./errorHandler');
+const validateEmail = require('./validateEmail');
+const validatePassword = require('./validatePassword');
 
 module.exports = {
   errorHandler,
+  validateEmail,
+  validatePassword,
 };
