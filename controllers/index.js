0b48386ceeeb6f9a31c8bccf5eb2551decc5496a
LaizaBertelli <laizabertelli@hotmail.com>
2022-02-20 22:51:23 -0300

    feat: add ref for editTalker on its index

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const getTalkers = require('./getTalkers');
 const getTalkerById = require('./getTalkerById');
 const newlogin = require('./newLogin');
 const createTalker = require('./createTalker');
+const editTalker = require('./editTalker');
 
 module.exports = {
   getTalkers,
   getTalkerById,
   newlogin,
   createTalker,
+  editTalker,
 };
