fa59bb7456ab70bfa1b103a13372e9429321057c
LaizaBertelli <laizabertelli@hotmail.com>
2022-02-20 21:23:48 -0300

    fix: remove console.log

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -1,7 +1,6 @@
 module.exports = (req, res, next) => {
   try {
     const { authorization } = req.headers;
-    console.log(authorization)
     if (!authorization) {
       return res.status(401).json({ message: 'Token não encontrado' });
     }
