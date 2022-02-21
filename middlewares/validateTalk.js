c659fa11c8aae72cea7330e2e8a0540e77ab7274
LaizaBertelli <laizabertelli@hotmail.com>
2022-02-20 22:53:07 -0300

    refactor: refacor validateTalk

--- middlewares/validateTalk.js
+++ middlewares/validateTalk.js
@@ -1,14 +1,18 @@
 // I didn't use try/catch and arrow function because of lint :/
-function validateExistence(talk, res) {
-  if (!talk || !talk.rate || !talk.watchedAt) {
+function validateExistence(talk) {
+  if (!talk || (!talk.rate && talk.rate !== 0) || !talk.watchedAt) {
+    return true;
+  }
+  return false;
+}
+module.exports = (req, res, next) => {
+  const { talk } = req.body;
+  const isNull = validateExistence(talk);
+  if (isNull) {
     return res.status(400).json({
         message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
   }
-}
-module.exports = (req, res, next) => {
-  const { talk } = req.body;
-  validateExistence(talk, res);
   // This is not the best way to validate a date formatted as dd/mm/yyyy, but considering the tests of this project, it will do.
   const date = talk.watchedAt.includes('/');
   if (!date) {
