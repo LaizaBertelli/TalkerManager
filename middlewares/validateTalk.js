581210056b09e40a266e7b47c9c4df6f49a03f60
LaizaBertelli <laizabertelli@hotmail.com>
2022-02-20 21:13:13 -0300

    feat: implements validationRalk.js

---
+++ middlewares/validateTalk.js
@@ -0,0 +1,25 @@
+module.exports = (req, res, next) => {
+  try {
+    const { talk } = req.body;
+    if (!talk) {
+      return res.status(400).json({  message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios'});
+    }
+    if (!talk.watchedAt) {
+      return res.status(400).json({  message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios'});
+    }
+    //This is not the best way to validate a date formatted as dd/mm/yyyy, but considering the tests of this project, it will do.
+    const date = talk.watchedAt.includes('/');
+    if (!date) {
+      return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+    }
+    if (!talk.rate) {
+      return res.status(400).json({  message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios'});
+    }
+    if (talk.rate < 1 || talk.rate > 5) {
+      return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    }
+    return next();
+  } catch(e) {
+    return next(e);
+  }
+};
