249cd1ed9f5b50078691d5dabc67b663d026bc5b
LaizaBertelli <laizabertelli@hotmail.com>
2022-02-20 21:13:49 -0300

    feat: add middlewares ref to index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,17 @@
 const errorHandler = require('./errorHandler');
 const validateEmail = require('./validateEmail');
 const validatePassword = require('./validatePassword');
+const validateToken = require('./validateToken');
+const validateName = require('./validateName');
+const validateAge = require('./validateAge');
+const validateTalk = require('./validateTalk');
 
 module.exports = {
   errorHandler,
   validateEmail,
   validatePassword,
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
 };
